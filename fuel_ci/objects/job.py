faf36f068a35edffb1b67923d91f83865691c3ac
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2014-12-25 12:00:32 +0300

    License header

--- fuel_ci/objects/job.py
+++ fuel_ci/objects/job.py
@@ -13,29 +13,3 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-"""
-Driver for creating tar archives
-"""
-
-import tarfile
-
-
-def pack(obj):
-    """Pack object into tar archive
-
-    :param obj: (Artifact) object with "name" and "archive" attributes
-    """
-    tar = tarfile.open(obj.name, "w")
-    for path in obj.content:
-        # TODO: check if file or directory
-        tar.add(path)
-    tar.close()
-    obj.archive = obj.name
-
-
-def unpack(obj):
-    """Unpack tar archive
-
-    :param obj: object with "archive" attribute
-    """
-    pass
