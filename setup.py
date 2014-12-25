d3d03c2ac4dbb1188553eacedf8d24bafc59b067
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2014-12-25 11:51:38 +0300

    Infrastructure changes.

--- setup.py
+++ setup.py
@@ -1,27 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import find_packages
-from setuptools import setup
+import setuptools
+
+import pbr
+import pbr.packaging
 
 
-name = 'fuel-ci'
-version = '0.0.1'
+# this monkey patch is to avoid appending git version to version
+pbr.packaging._get_version_from_git = lambda pre_version: pre_version
 
 
-if __name__ == "__main__":
-    setup(
-        name=name,
-        version=version,
-        description='Fuel CI Build System',
-        classifiers=[
-            "Programming Language :: Python",
-            "Topic :: Internet :: WWW/HTTP",
-        ],
-        packages=find_packages(),
-        include_package_data=True,
-        entry_points={
-            'console_scripts': [
-                'fci = fuel_ci.cli:main',
-            ],
-        }
-    )
+setuptools.setup(
+    setup_requires=['pbr'],
+    pbr=True)
